0b515a22536b81f151d9074ea3d5e228f27f37c4
facualderete <facualderete@gmail.com>
2013-10-16 17:26:18 -0300

    Part of the initial refactoring, involving the separation of every "proxy" module into "client" and "server".

--- thorium/src/main/java/ar/thorium/dispatcher/implementation/NioClientDispatcher.java
+++ thorium/src/main/java/ar/thorium/dispatcher/implementation/NioClientDispatcher.java
@@ -1,12 +1,12 @@
 package ar.thorium.dispatcher.implementation;
 
 import ar.thorium.dispatcher.SelectorGuard;
-import ar.thorium.handler.ClientEventHandler;
+import ar.thorium.handler.*;
 import ar.thorium.handler.ClientHandlerAdapter;
 import ar.thorium.queues.InputQueueFactory;
 import ar.thorium.queues.OutputQueueFactory;
+import ar.thorium.queues.exceptions.QueueBuildingException;
 import ar.thorium.utils.ChannelFacade;
-
 import java.io.IOException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
@@ -21,16 +21,59 @@ public class NioClientDispatcher<T extends ClientEventHandler>
 
     @Override
     public ChannelFacade registerChannel(SelectableChannel channel, T handler) throws IOException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        channel.configureBlocking(false);
+
+        ClientHandlerAdapter<T> adapter;
+        try {
+            adapter = new ClientHandlerAdapter<T>(this, inputQueueFactory.newInputQueue(), outputQueueFactory.newOutputQueue(), handler);
+        } catch (QueueBuildingException e) {
+            throw new IOException(e);
+        }
+
+        adapter.registering();
+
+        acquireSelector();
+
+        try {
+            SelectionKey key = channel.register(getSelector(), SelectionKey.OP_READ,
+                    adapter);
+
+            adapter.setKey(key);
+            adapter.registered();
+
+            return adapter;
+        } finally {
+            releaseSelector();
+        }
     }
 
     @Override
     public void unregisterChannel(ChannelFacade key) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if (!(key instanceof ClientHandlerAdapter<?>)) {
+            throw new IllegalArgumentException("Not a valid registration token");
+        }
+
+        @SuppressWarnings("unchecked")
+        ClientHandlerAdapter<T> adapter = (ClientHandlerAdapter<T>) key;
+        SelectionKey selectionKey = adapter.key();
+
+        acquireSelector();
+
+        try {
+            adapter.unregistering();
+            selectionKey.cancel();
+        } finally {
+            releaseSelector();
+        }
+
+        adapter.unregistered();
     }
 
     @Override
     protected void invokeHandler(ClientHandlerAdapter<T> adapter, SelectionKey key) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        adapter.prepareToRun(key);
+        adapter.key().interestOps(0);
+
+        executor.execute(new HandlerFutureTask<T , ChannelFacade, ClientHandlerAdapter<T>>(adapter, this, key));
     }
 }
--- thorium/src/main/java/ar/thorium/handler/ServerEventHandler.java
+++ thorium/src/main/java/ar/thorium/handler/ServerEventHandler.java
@@ -1,10 +1,10 @@
 package ar.thorium.handler;
 
 import ar.thorium.utils.ChannelFacade;
-
 import java.nio.ByteBuffer;
 
 public interface ServerEventHandler extends EventHandler{
 	ByteBuffer nextMessage(ChannelFacade channelFacade);
 	void handleInput(ByteBuffer message, ChannelFacade channelFacade);
+    void handleConnection(ChannelFacade facade);
 }
