98ba4fe3796c19339c903fdef1185995d994ac1b
facualderete <facualderete@gmail.com>
2013-11-02 19:17:56 -0300

    Se empiezan a hacer los cambios necesarios en hyttrop

--- hyttrop/src/main/java/ar/edu/itba/pdc/handler/HttpEventHandler.java
+++ hyttrop/src/main/java/ar/edu/itba/pdc/handler/HttpEventHandler.java
@@ -2,13 +2,35 @@ package ar.edu.itba.pdc.handler;
 
 import ar.thorium.handler.EventHandler;
 import ar.thorium.utils.ChannelFacade;
+import ar.thorium.utils.Message;
 
 import java.nio.ByteBuffer;
 
-public interface HttpEventHandler extends EventHandler {
-    ByteBuffer nextMessage(ChannelFacade channelFacade);
-    void handleInput(ByteBuffer message, ChannelFacade channelFacade);
-    void handleConnection(ChannelFacade facade);
-    void handleWrite(ChannelFacade channelFacade);
-    void handleRead(ChannelFacade channelFacade, Message message);
+public class HttpEventHandler implements EventHandler {
+
+
+    @Override
+    public void handleRead(ChannelFacade channelFacade, Message message) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void handleWrite(ChannelFacade channelFacade) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void handleConnection(ChannelFacade channelFacade) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void stopped(ChannelFacade channelFacade) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void stopping(ChannelFacade channelFacade) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
